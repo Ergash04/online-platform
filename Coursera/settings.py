d07e127df750d24c7b448629526d42e452110990
Ergash04 <93190932+Ergash04@users.noreply.github.com>
2022-08-02 08:10:09 +0000

    Update settings.py

--- Coursera/settings.py
+++ Coursera/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'online_platform',
         'USER': 'postgres',
-        'PASSWORD': '@er14312',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
